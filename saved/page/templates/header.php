75a96c870184cf8b140346cbe25d10108bbd0759
tazthemaniac <pontus_nilsson_92@hotmail.com>
2016-11-09 01:09:06 +0100

    Updated font links, turned index.php example file to a readme

--- saved/page/templates/header.php
+++ saved/page/templates/header.php
@@ -12,7 +12,7 @@
     <link rel="icon" type="image/png" href="favicon.png">
 
     <!-- Google font, not required for webdeq to work -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,700" rel="stylesheet"> 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
     <link rel="stylesheet" href="resources/style.css">
